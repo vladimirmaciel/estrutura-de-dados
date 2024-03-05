fd9f9421fb0a8e0cf5424cd6159dacdec9de383e
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-03-05 08:53:55 -0300

    arquivo .girignore

---
+++ arvore-bianaria/suporte.py
@@ -0,0 +1,13 @@
+from tree import BinaryTree, Node
+
+if __name__ == '__main__':
+    tree = BinaryTree()
+    n1 = Node('a')
+    n2 = Node('+')
+    n3 = Node('*')
+    n4 = Node('b')
+    n5 = Node('-')
+    n6 = Node('/')
+    n7 = Node('c')
+    n8 = Node('d')
+    n9 = Node('e')
