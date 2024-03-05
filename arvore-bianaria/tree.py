82440ab5ddea4ceff59b4d11e12444ee84e690c2
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-03-05 08:49:58 -0300

    escrevendo árvoe binaria

---
+++ arvore-bianaria/tree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
+
+
+class BinaryTree:
+    def __init__(self, data=None):
+        if data:
+            node = Node(data)
+            self.root = node
+        else:
+            self.root = None
